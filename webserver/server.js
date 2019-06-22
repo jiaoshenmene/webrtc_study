18e466ec66905165a47c06f5ca9f925bd65068d8
杜甲 <815319775@qq.com>
2019-06-22 09:38:20 +0800

    添加证书

--- webserver/server.js
+++ webserver/server.js
@@ -5,15 +5,18 @@ var https = require('https');
 var fs = require('fs');
 
 var express = require('express');
+
 var serveIndex = require('serve-index');
 
 var app = express();
+app.use(serveIndex('./public'));
+app.use(express.static('./public'));
 var http_server = http.createServer(app);
 http_server.listen(81, '0.0.0.0');
 
 var options = {
-key:fs.readFileSync('./cert/'),
-cert:fs.readFileSync('./cert/'),
+    key: fs.readFileSync('./cert/2_www.mcyyx.com.key'),
+    cert: fs.readFileSync('./cert/1_www.mcyyx.com_bundle.crt'),
 }
 
 var https_server = https.createServer(options, app);
