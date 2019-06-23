567360f6e4d3b6434505f274b9fb962027fb6ca6
杜甲 <815319775@qq.com>
2019-06-23 19:49:46 +0800

    增加http支持的服务器

--- webserver/server.js
+++ webserver/server.js
@@ -23,10 +23,10 @@ log4js.configure({
         }
     },
     categories: {
-       default: {
-          appenders: ['file'],
-          level: 'debug'
-       }
+        default: {
+            appenders: ['file'],
+            level: 'debug'
+        }
     }
 });
 
@@ -37,57 +37,98 @@ app.use(serveIndex('./public'));
 app.use(express.static('./public'));
 
 
-
 //http server
 var http_server = http.createServer(app);
 http_server.listen(80, '0.0.0.0');
 
 var options = {
-	key: fs.readFileSync('./cert/2394334_www.mancangyun.com.key'),
-	cert: fs.readFileSync('./cert/2394334_www.mancangyun.com.pem'),
+    key: fs.readFileSync('./cert/2394334_www.mancangyun.com.key'),
+    cert: fs.readFileSync('./cert/2394334_www.mancangyun.com.pem'),
 }
 
 //https server
 var https_server = https.createServer(options, app);
 var io = socketIo.listen(https_server);
 
-io.sockets.on('connection', (socket)=> {
+var sockio = socketIo.listen(http_server);
 
-	socket.on('message', (room, data)=>{
-		socket.to(room).emit('message',room, data);
-	});
+//connection
+sockio.sockets.on('connection', (socket) => {
 
-	socket.on('join', (room)=>{
-		socket.join(room);
-		var myRoom = io.sockets.adapter.rooms[room];
-		var users = (myRoom)? Object.keys(myRoom.sockets).length : 0;
-		logger.debug('the user number of room is: ' + users);
+    socket.on('message', (room, data) => {
+        socket.to(room).emit('message', room, data);
+    });
 
-		if(users < USERCOUNT){
-			socket.emit('joined', room, socket.id); //发给除自己之外的房间内的所有人
-			if(users > 1){
-				socket.to(room).emit('otherjoin', room, socket.id);
-			}
+    socket.on('join', (room) => {
+        socket.join(room);
+        var myRoom = io.sockets.adapter.rooms[room];
+        var users = (myRoom) ? Object.keys(myRoom.sockets).length : 0;
+        logger.debug('the user number of room is: ' + users);
 
-		}else{
-			socket.leave(room);
-			socket.emit('full', room, socket.id);
-		}
-		//socket.emit('joined', room, socket.id); //发给自己
-		//socket.broadcast.emit('joined', room, socket.id); //发给除自己之外的这个节点上的所有人
-		//io.in(room).emit('joined', room, socket.id); //发给房间内的所有人
-	});
+        if (users < USERCOUNT) {
+            socket.emit('joined', room, socket.id); //发给除自己之外的房间内的所有人
+            if (users > 1) {
+                socket.to(room).emit('otherjoin', room, socket.id);
+            }
 
-	socket.on('leave', (room)=>{
-		var myRoom = io.sockets.adapter.rooms[room];
-		var users = (myRoom)? Object.keys(myRoom.sockets).length : 0;
-		logger.debug('the user number of room is: ' + (users-1));
-		//socket.emit('leaved', room, socket.id);
-		//socket.broadcast.emit('leaved', room, socket.id);
-		socket.to(room).emit('bye', room, socket.id);
-		socket.emit('leaved', room, socket.id);
-		//io.in(room).emit('leaved', room, socket.id);
-	});
+        } else {
+            socket.leave(room);
+            socket.emit('full', room, socket.id);
+        }
+        //socket.emit('joined', room, socket.id); //发给自己
+        //socket.broadcast.emit('joined', room, socket.id); //发给除自己之外的这个节点上的所有人
+        //io.in(room).emit('joined', room, socket.id); //发给房间内的所有人
+    });
+
+    socket.on('leave', (room) => {
+        var myRoom = io.sockets.adapter.rooms[room];
+        var users = (myRoom) ? Object.keys(myRoom.sockets).length : 0;
+        logger.debug('the user number of room is: ' + (users - 1));
+        //socket.emit('leaved', room, socket.id);
+        //socket.broadcast.emit('leaved', room, socket.id);
+        socket.to(room).emit('bye', room, socket.id);
+        socket.emit('leaved', room, socket.id);
+        //io.in(room).emit('leaved', room, socket.id);
+    });
+});
+
+io.sockets.on('connection', (socket) => {
+
+    socket.on('message', (room, data) => {
+        socket.to(room).emit('message', room, data);
+    });
+
+    socket.on('join', (room) => {
+        socket.join(room);
+        var myRoom = io.sockets.adapter.rooms[room];
+        var users = (myRoom) ? Object.keys(myRoom.sockets).length : 0;
+        logger.debug('the user number of room is: ' + users);
+
+        if (users < USERCOUNT) {
+            socket.emit('joined', room, socket.id); //发给除自己之外的房间内的所有人
+            if (users > 1) {
+                socket.to(room).emit('otherjoin', room, socket.id);
+            }
+
+        } else {
+            socket.leave(room);
+            socket.emit('full', room, socket.id);
+        }
+        //socket.emit('joined', room, socket.id); //发给自己
+        //socket.broadcast.emit('joined', room, socket.id); //发给除自己之外的这个节点上的所有人
+        //io.in(room).emit('joined', room, socket.id); //发给房间内的所有人
+    });
+
+    socket.on('leave', (room) => {
+        var myRoom = io.sockets.adapter.rooms[room];
+        var users = (myRoom) ? Object.keys(myRoom.sockets).length : 0;
+        logger.debug('the user number of room is: ' + (users - 1));
+        //socket.emit('leaved', room, socket.id);
+        //socket.broadcast.emit('leaved', room, socket.id);
+        socket.to(room).emit('bye', room, socket.id);
+        socket.emit('leaved', room, socket.id);
+        //io.in(room).emit('leaved', room, socket.id);
+    });
 
 });
 
